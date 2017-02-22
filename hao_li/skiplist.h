447c5ec3ca25eded829daf7ff78985dd1ff136fd
Hao Li <lihao98722@gmail.com>
2017-02-21 23:21:38 -0500

    finished baseline skiplist

--- hao_li/skiplist.h
+++ hao_li/skiplist.h
@@ -1,3 +1,7 @@
+/**
+ * This SKipList is an implementation of <ftp://ftp.cs.umd.edu/pub/skipLists/skiplists.pdf>
+ */
+
 #include <iostream>
 #include <vector>
 #include <cstdlib>
@@ -24,9 +28,31 @@ class SkipList
 
 public:
     SkipList(): _cur_height(0), _head(new Entry()) {}
-    ~SkipList() { delete _head;}
+    ~SkipList()
+    {
+        auto curr = _head->forwards[0];
+        while (curr)
+        {
+            auto temp = curr;
+            curr = curr->forwards[0];
+            delete temp;
+        }
+        delete _head;
+    }
 
-    Entry find(KeyType target)
+    void display()
+    {
+        for (auto h = _cur_height; h >= 0; --h)
+        {
+            for (auto curr = _head->forwards[h]; curr; curr = curr->forwards[h])
+            {
+                std::cout << "(" << curr->key << ", " << curr->val << ") -> ";
+            }
+            std::cout << std::endl;
+        }
+    }
+
+    Entry* find(KeyType target)
     {
         auto cur = _head;
         for (int level = _cur_height; level >= 0; --level)
@@ -40,8 +66,80 @@ public:
         return cur->key == target ? cur : nullptr;
     }
 
-    bool insert(KeyType key, ValueType val);
-    bool remove(KeyType key);
+    bool insert(KeyType key, ValueType val)
+    {
+        std::vector<Entry*> updates(MAX_LEVEL, nullptr);
+        auto curr = _head;
+        for (int h = _cur_height; h >= 0; --h)
+        {
+            for (;curr->forwards[h] && curr->forwards[h]->key < key; curr = curr->forwards[h]);
+            updates[h] = curr;
+        }
+
+        curr = curr->forwards[0];
+        if (curr && curr->key == key) {
+            curr->val = val;
+        }
+        else {
+            int new_h = rand_level();
+            if (new_h > _cur_height) {
+                for (int h = _cur_height + 1; h <= new_h; ++h)
+                {
+                    updates[h] = _head;
+                }
+                _cur_height = new_h;
+            }
+            curr = new Entry(key, val);
+            for (int h = 0; h <= _cur_height; ++h)
+            {
+                curr->forwards[h] = updates[h]->forwards[h];
+                updates[h]->forwards[h] = curr;
+            }
+        }
+    }
+
+    bool remove(KeyType target)
+    {
+        std::vector<Entry*> updates(MAX_LEVEL, nullptr);
+        auto curr = _head;
+        for (int h = _cur_height; h >= 0; --h)
+        {
+            for (;curr->forwards[h] && curr->forwards[h]->key < target; curr = curr->forwards[h]);
+            updates[h] = curr;
+        }
+
+        curr = curr->forwards[0];
+        if (curr->key == target) {
+            for (int h = 0; h <= _cur_height; ++h)
+            {
+                if (updates[h]->forwards[h] != curr) {
+                    break;
+                }
+                updates[h]->forwards[h] = curr->forwards[h];
+            }
+            delete curr;
+            while (_cur_height > 0 && _head->forwards[_cur_height] == nullptr)
+            {
+                --_cur_height;
+            }
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+
+private:
+    int rand_level()
+    {
+        int level = 1;
+        while (double(std::rand()) / RAND_MAX > 0.5)
+        {
+            ++level;
+        }
+        return level;
+    }
 
 private:
     int _cur_height;
