9f0aac9f72e0deec9317ce29dcaf86f1e56b4252
Hao Li <lihao98722@gmail.com>
2017-02-24 19:26:39 +0000

    updated remove function signature; updated function naming convention

--- src/include/index/skiplist.h
+++ src/include/index/skiplist.h
@@ -15,14 +15,11 @@
 #include <iostream>
 #include <vector>
 #include <cstdlib>
-#include <atomic>
 
 #define SKIPLIST_TEMPLATE_ARGUMENTS                                       \
   template <typename KeyType, typename ValueType, typename KeyComparator, \
             typename KeyEqualityChecker, typename ValueEqualityChecker>
 
-#define SKIPLSIT_ENTRY_TEMPLATE_ARGUMENTS template \
-  <typename KeyType, typename ValueType>
 
 namespace peloton {
 namespace index {
@@ -30,7 +27,7 @@ namespace skiplist{
 
 constexpr int MAX_LEVEL = 32;
 
-SKIPLSIT_ENTRY_TEMPLATE_ARGUMENTS
+template <typename KeyType, typename ValueType>
 class SkipListEntry
 {
 public:
@@ -41,8 +38,6 @@ public:
     std::vector<SkipListEntry<KeyType, ValueType>*> forwards{MAX_LEVEL, nullptr};
 };
 
-SKIPLSIT_ENTRY_TEMPLATE_ARGUMENTS
-class EpochManager;
 
 template <typename KeyType,
           typename ValueType,
@@ -77,7 +72,7 @@ public:
         delete _head;
     }
 
-    void display()
+    void Display()
     {
         for (auto h = _curr_height; h >= 0; --h)
         {
@@ -89,37 +84,37 @@ public:
         }
     }
 
-    Entry* find(KeyType target)
+    Entry* Find(KeyType target_key)
     {
         auto curr = _head;
         for (int level = _curr_height; level >= 0; --level)
         {
-            while (curr->forwards[level] && _key_cmp_obj(curr->forwards[level]->key, target))
+            while (curr->forwards[level] && _key_cmp_obj(curr->forwards[level]->key, target_key))
             {
                 curr = curr->forwards[level];
             }
         }
         curr = curr->forwards[0];
-        return curr && _key_eq_obj(curr->key, target) ? curr : nullptr;
+        return curr && _key_eq_obj(curr->key, target_key) ? curr : nullptr;
     }
 
-    bool insert(KeyType key, ValueType val)
+    bool Insert(KeyType inserted_key, ValueType inserted_val)
     {
         std::vector<Entry*> updates(MAX_LEVEL, nullptr);
         auto curr = _head;
         for (int h = _curr_height; h >= 0; --h)
         {
-            for (;curr->forwards[h] && _key_cmp_obj(curr->forwards[h]->key, key); curr = curr->forwards[h]);
+            for (;curr->forwards[h] && _key_cmp_obj(curr->forwards[h]->key, inserted_key); curr = curr->forwards[h]);
             updates[h] = curr;
         }
 
         curr = curr->forwards[0];
         // already exist
-        if (curr && _key_eq_obj(curr->key, key)) {
+        if (curr && _key_eq_obj(curr->key, inserted_key)) {
             return false;
         }
         else {
-            int new_h = rand_level();
+            int new_h = RandLevel();
             if (new_h > _curr_height) {
                 for (int h = _curr_height + 1; h <= new_h; ++h)
                 {
@@ -127,7 +122,7 @@ public:
                 }
                 _curr_height = new_h;
             }
-            curr = new Entry(key, val);
+            curr = new Entry(inserted_key, inserted_val);
             for (int h = 0; h <= new_h; ++h)
             {
                 curr->forwards[h] = updates[h]->forwards[h];
@@ -137,18 +132,19 @@ public:
         }
     }
 
-    bool remove(KeyType target)
+
+    bool Remove(KeyType target_key, ValueType target_val)
     {
         std::vector<Entry*> updates(MAX_LEVEL, nullptr);
         auto curr = _head;
         for (int h = _curr_height; h >= 0; --h)
         {
-            for (;curr->forwards[h] && _key_cmp_obj(curr->forwards[h]->key, target); curr = curr->forwards[h]);
+            for (;curr->forwards[h] && _key_cmp_obj(curr->forwards[h]->key, target_key); curr = curr->forwards[h]);
             updates[h] = curr;
         }
 
         curr = curr->forwards[0];
-        if (curr && _key_eq_obj(curr->key, target)) {
+        if (curr && _key_eq_obj(curr->key, target_key) && _val_eq_obj(curr->val, target_val)) {
             for (int h = 0; h <= _curr_height; ++h)
             {
                 if (updates[h]->forwards[h] != curr) {
@@ -168,10 +164,9 @@ public:
         }
     }
 
-    EpochManager<KeyType, ValueType> epoch_manager;
 
 private:
-    int rand_level()
+    int RandLevel()
     {
         int r = std::rand() & ((1L << MAX_LEVEL) - 1);
         int level = 0;
@@ -190,114 +185,6 @@ private:
     const KeyEqualityChecker _key_eq_obj;
     const ValueEqualityChecker _val_eq_obj;
 };
-
-SKIPLSIT_ENTRY_TEMPLATE_ARGUMENTS
-class EpochManager
-{
-public:
-
-  constexpr static int GC_INTERVAL = 50;
-
-  struct GarbageNode {
-    const SkipListEntry<KeyType, ValueType> *entry_p;
-    GarbageNode *next_p;
-  };
-
-  class EpochNode {
-   public:
-    std::atomic<int> active_thread_count;
-    std::atomic<GarbageNode *> garbage_list_p;
-    EpochNode *next_p;
-
-    EpochNode();
-  };
-
-  EpochNode *head_epoch_p;
-  EpochNode *current_epoch_p;
-  std::atomic<bool> exited_flag;
-
-  EpochManager();
-  ~EpochManager();
-
-  void AddGarbageNode(const SkipListEntry<KeyType, ValueType> *entry_p);
-  inline EpochNode *JoinEpoch();
-  inline void LeaveEpoch(EpochNode *epoch_p);
-
-  void PerformGarbageCollection();
-  void CreateNewEpoch();
-  void ClearEpoch();
-
-  void StartThread();
-
-private:
-  void FreeEpochDeltaChain(const SkipListEntry<KeyType, ValueType> *entry_p);
-
-  void ThreadFunc();
-};
-
-SKIPLSIT_ENTRY_TEMPLATE_ARGUMENTS
-EpochManager<KeyType, ValueType>::EpochNode::EpochNode() {
-  active_thread_count = 0;
-  garbage_list_p = nullptr;
-  next_p = nullptr;
-}
-
-SKIPLSIT_ENTRY_TEMPLATE_ARGUMENTS
-EpochManager<KeyType, ValueType>::EpochManager() {
-  head_epoch_p = new EpochNode();
-
-  current_epoch_p = head_epoch_p;
-
-  exited_flag.store(false);
-
-  return;
-}
-
-SKIPLSIT_ENTRY_TEMPLATE_ARGUMENTS
-EpochManager<KeyType, ValueType>::~EpochManager() {
-  exited_flag.store(true);
-  current_epoch_p = nullptr;
-
-  auto *epoch_node_p = head_epoch_p;
-  while (epoch_node_p != nullptr) {
-    auto prev_node_p = epoch_node_p;
-    epoch_node_p = epoch_node_p->next_p;
-    delete prev_node_p;
-  }
-  head_epoch_p = nullptr;
-
-  EpochManager<KeyType, ValueType>::ClearEpoch();
-  assert(head_epoch_p == nullptr);
-
-  return;
-}
-
-SKIPLSIT_ENTRY_TEMPLATE_ARGUMENTS
-void EpochManager<KeyType, ValueType>::CreateNewEpoch() {
-  EpochNode *epoch_node_p = new EpochNode();
-  current_epoch_p->next_p = epoch_node_p;
-  current_epoch_p = epoch_node_p;
-  return;
-}
-
-SKIPLSIT_ENTRY_TEMPLATE_ARGUMENTS
-void EpochManager<KeyType, ValueType>::ClearEpoch() {
-  // TODO: to be implemented.
-  return;
-}
-
-SKIPLSIT_ENTRY_TEMPLATE_ARGUMENTS
-inline typename EpochManager<KeyType, ValueType>::EpochNode *EpochManager<KeyType, ValueType>::JoinEpoch() {
-  EpochNode *epoch_p = current_epoch_p;
-  epoch_p->active_thread_count.fetch_add(1);
-  return epoch_p;
-}
-
-SKIPLSIT_ENTRY_TEMPLATE_ARGUMENTS
-inline void EpochManager<KeyType, ValueType>::LeaveEpoch(EpochNode *epoch_p) {
-  epoch_p->active_thread_count.fetch_sub(1);
-  return;
-}
 
 }
 }
--- test/index/skiplist_test.cpp
+++ test/index/skiplist_test.cpp
@@ -28,31 +28,10 @@ TEST_F(SkipListTests, BasicTest) {
   index::skiplist::SkipList<int, int> *sl = new index::skiplist::SkipList<int, int>();
   int key = 1;
   int val = 1000;
-  sl->insert(key, val);
-  auto found = sl->find(key);
+  sl->Insert(key, val);
+  auto found = sl->Find(key);
   EXPECT_EQ(val, found->val);
   delete sl;
-}
-
-TEST_F(SkipListTests, EpochJoinAndExit) {
-  index::skiplist::SkipList<int, int, int ,int ,int> list;
-  auto &epoch_manager = list.epoch_manager;
-
-  // Epoch #1
-  epoch_manager.CreateNewEpoch();
-  auto epoch1 = epoch_manager.JoinEpoch();
-  auto epoch2 = epoch_manager.JoinEpoch();
-  EXPECT_EQ(epoch1, epoch2);
-  EXPECT_EQ(epoch1->active_thread_count, 2);
-
-  // Epoch #2
-  epoch_manager.CreateNewEpoch();
-  auto epoch3 = epoch_manager.JoinEpoch();
-  EXPECT_EQ(epoch1->active_thread_count, 2);
-  EXPECT_EQ(epoch3->active_thread_count, 1);
-
-  epoch_manager.LeaveEpoch(epoch1);
-  EXPECT_EQ(epoch2->active_thread_count, 1);
 }
 
 }  // End test namespace
